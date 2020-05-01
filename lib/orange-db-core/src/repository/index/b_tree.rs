ae5a1ffb79b4b9f7be77c0cf4cddeba52632ebcf
Alexandre RICCIARDI <alexandre.ricciardi@liteapps.fr>
2020-05-01 07:31:06 +0200

    misc

--- lib/orange-db-core/src/repository/index/b_tree.rs
+++ lib/orange-db-core/src/repository/index/b_tree.rs
@@ -1,33 +1,6 @@
-use super::super::byte_utils::*;
 use orange_db_binary_serde_traits::BinarySer;
 use orange_db_binary_serde_traits::BinaryDeser;
-
-fn inc_offset(o: usize) -> usize {
-    std::mem::size_of::<u64>() + o
-}
-
-macro_rules! to_bytes_impl {
-    [$bytes:ident, $($field:expr),*] => {
-        let mut offset = 0usize;
-        let upper_bound = inc_offset(offset);
-        $($bytes[offset..inc_offset(offset)].clone_from_slice(&u64_to_bytes($field));offset += upper_bound;)*
-    };
-}
-
-
-fn deser_and_inc(bytes: &[u8], offset: usize) -> u64 {
-    let upper_bound = inc_offset(offset);
-    let res = u64_from_bytes(&bytes[offset..upper_bound]);
-    res
-}
-
-macro_rules! from_bytes_impl {
-    [$bytes:ident, $s_name:ty, $($field:expr),*] => {
-        let mut offset = 0;
-        let res = $s_name {$($field: deser_and_inc($bytes, offset)),*};
-        res
-    };
-}
+use super::super::io::file_access::FileAccess;
 
 pub struct BNode {
 
@@ -45,10 +18,21 @@ pub struct BNodeRecord {
     pub next: u64,
 }
 
-fn to_bytes(bnr: &BNodeRecord) -> [u8; 64] {
-    let mut bytes = [0u8; 64];
-    to_bytes_impl![bytes, bnr.ptr_0, bnr.slot_0];
-    bytes
+
+pub struct BTreeIndex {
+    file_access: FileAccess,
+}
+
+impl BTreeIndex {
+    pub fn new(file: &str) -> Self {
+        BTreeIndex{file_access: FileAccess::new(file)}
+    }
+
+    pub fn search(&mut self, value: u64) -> Option<BNodeRecord> {
+        let mut data = [0u8; 64];
+        self.file_access.read_at(0, &mut  data);
+        Some(BNodeRecord::deser(&data.to_vec()))
+    }
 }
 
 #[cfg(test)]
