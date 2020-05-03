949e774c27bdb83633f2920ec03bba8eb2d26e3d
Alexandre RICCIARDI <alexandre.ricciardi@liteapps.fr>
2020-05-03 04:32:33 +0200

    binarize

--- lib/orange-db-core/src/repository/index/b_tree.rs
+++ lib/orange-db-core/src/repository/index/b_tree.rs
@@ -13,7 +13,6 @@ pub struct BNodeRecord {
     pub ptr_2: u64,
     pub slot_2: u64,
     pub ptr_3: u64,
-    pub next: u64,
 }
 
 pub struct BTreeIndex {
@@ -26,7 +25,7 @@ impl BTreeIndex {
     }
 
     pub fn search(&mut self, value: u64) -> Option<BNodeRecord> {
-        let mut data = [0u8; 64];
+        let mut data = [0u8; 56];
         self.file_access.read_at(0, &mut  data);
         Some(BNodeRecord::from_bytes(data))
     }
@@ -37,7 +36,7 @@ mod test_b_tree {
     use super::*;
     #[test]
     fn test_ser() {
-        let rec = BNodeRecord{ptr_0: {1 + 2 + 3}, ptr_1: 12, ptr_2: 33, ptr_3: 99, slot_0: 1899988, slot_1: 214975, slot_2: 22223, next: 3};
+        let rec = BNodeRecord{ptr_0: {1 + 2 + 3}, ptr_1: 12, ptr_2: 33, ptr_3: 99, slot_0: 1899988, slot_1: 214975, slot_2: 22223};
         let data = rec.to_bytes();
         let deser = BNodeRecord::from_bytes(data);
         assert_eq!(rec, deser)
