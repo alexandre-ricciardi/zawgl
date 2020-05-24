63a101ed33797b4243504d3fbbc9f1b097f65bae
Alexandre RICCIARDI <alexandre.ricciardi@liteapps.fr>
2020-05-24 05:13:37 +0200

    pager

--- lib/orange-db-core/src/repository/pager/mod.rs
+++ lib/orange-db-core/src/repository/pager/mod.rs
@@ -13,47 +13,80 @@ pub enum PagerError {
 pub type PagerResult = std::result::Result<PageId, PagerError>;
 
 
-const PAGE_HEADER_SIZE: usize = 1;
+const PAGE_HEADER_SIZE: usize = 8;
 const PAGE_SIZE: usize = 4096;
-const PAGE_PAYLOAD_SIZE: usize = PAGE_SIZE - PAGE_HEADER_SIZE;
+const PAGE_FREE_LIST_PTR_SIZE: usize = 4;
 
-pub struct Page {
-    header: u8,
-    payload: [u8; PAGE_PAYLOAD_SIZE],
+fn compute_freelist_len(record_size: usize) -> usize {
+    (PAGE_SIZE - PAGE_HEADER_SIZE) / record_size
+}
+fn compute_freelist_size(record_size: usize) -> usize {
+    PAGE_FREE_LIST_PTR_SIZE * compute_freelist_len(record_size)
+}
+fn compute_header_size(record_size: usize) -> usize {
+    compute_freelist_size(record_size) + PAGE_HEADER_SIZE
+}
+fn compute_payload_size(record_size: usize) -> usize {
+    PAGE_SIZE - compute_header_size(record_size)
 }
 
-impl Page {
-    fn new() -> Self {
-        Page{header: 0, payload: [0u8; PAGE_PAYLOAD_SIZE]}
+pub struct Page<'a> {
+    data: &'a mut [u8; PAGE_SIZE],
+    record_size: usize,
+}
+
+impl <'a> Page<'a> {
+    fn new(data: &'a mut [u8; PAGE_SIZE], record_size: usize) -> Self {
+        Page{data: data, record_size: record_size}
     }
 
-    fn get_next_free_record_pos(&self) -> u8 {
-        self.header
+    fn get_next_freeblock_page(&self) -> PageId {
+        let mut bytes = [0u8; 8];
+        bytes.copy_from_slice(&self.data[..8]);
+        u64::from_be_bytes(bytes)
     }
 
-    fn set_next_free_record_pos(&mut self, pos: u8) {
-        self.header = pos;
+    fn set_next_freeblock_page(&mut self, pid: PageId) {
+        self.data.copy_from_slice(&pid.to_be_bytes());
     }
 
-    fn from_be_bytes(data: [u8; PAGE_SIZE]) -> Self {
-        let mut payload = [0u8; PAGE_PAYLOAD_SIZE];
-        payload.copy_from_slice(&data[PAGE_HEADER_SIZE..]);
-        Page{header: data[0], payload: payload}
+    fn get_list_free_page_record_ids(&self) -> Vec<PageRecordId> {
+        let mut res = Vec::new();
+        let freelist_len = compute_freelist_len(self.record_size);
+        let mut count = 0;
+        while count < freelist_len {
+            let beg_ptr = count * PAGE_FREE_LIST_PTR_SIZE;
+            let end_ptr = beg_ptr + PAGE_FREE_LIST_PTR_SIZE;
+            let page_record_id_bytes = [0u8; PAGE_FREE_LIST_PTR_SIZE];
+            page_record_id_bytes.copy_from_slice(&self.data[beg_ptr..end_ptr]);
+            let page_record_id = u32::from_be_bytes(page_record_id_bytes) as usize;
+            if page_record_id == 0 {
+                break;
+            }
+            res.push(page_record_id);
+            count += 1;
+        }
+        res
+    }
+
+    fn set_list_free_page_record_ids(&self, freelist: Vec<PageRecordId>) {
+        
     }
 }
 
 pub struct Pager {
     records_file: FileAccess,
     record_size: usize,
-    page_cache: HashMap<PageId, Box<Page>>
+    page_cache: HashMap<PageId, [u8; PAGE_SIZE]>
 }
 
 impl Pager {
     pub fn new(file: &str, record_size: usize) -> Self {
         Pager { records_file: FileAccess::new(file), record_size: record_size, page_cache: HashMap::new()}
     }
+    
     fn compute_page_id(&self, record_id: u64) -> (PageId, PageRecordId) {
-        let nb_records_per_page = PAGE_PAYLOAD_SIZE / self.record_size;
+        let nb_records_per_page = compute_payload_size(self.record_size) / self.record_size;
         (record_id / nb_records_per_page as u64, (record_id % nb_records_per_page as u64) as usize)
     }
     pub fn save(&mut self, id: RecordId, data: &[u8]) -> PagerResult {
@@ -62,15 +95,14 @@ impl Pager {
         if page_begin_pos > self.records_file.get_file_len() - PAGE_SIZE as u64 {
             Err(PagerError::FileOverflow)
         } else {
-            let location_in_page = location.1 * self.record_size;
+            let location_in_page = location.1 * self.record_size + PAGE_HEADER_SIZE;
             if let Some(page) = self.page_cache.get(&location.0) {
-                page.payload[location_in_page..location_in_page + self.record_size].copy_from_slice(data);
+                page[location_in_page..location_in_page + self.record_size].copy_from_slice(data);
             } else {
                 let mut page_data = [0u8; PAGE_SIZE];
                 self.records_file.read_at(page_begin_pos, &mut page_data);
-                let mut page = Box::new(Page::from_be_bytes(page_data));
-                page.payload[location_in_page..location_in_page + self.record_size].copy_from_slice(data);
-                self.page_cache.insert(location.0, page);
+                page_data[location_in_page..location_in_page + self.record_size].copy_from_slice(data);
+                self.page_cache.insert(location.0, page_data);
             }
             Ok(location.0)
         }
@@ -83,13 +115,12 @@ impl Pager {
         } else {
             let location_in_page = location.1 * self.record_size;
             if let Some(page) = self.page_cache.get(&location.0) {
-                data.copy_from_slice(&page.payload[location_in_page..location_in_page + self.record_size]);
+                data.copy_from_slice(&page[location_in_page..location_in_page + self.record_size]);
             } else {
                 let mut page_data = [0u8; PAGE_SIZE];
                 self.records_file.read_at(page_begin_pos, &mut page_data);
-                let mut page = Box::new(Page::from_be_bytes(page_data));
-                data.copy_from_slice(&page.payload[location_in_page..location_in_page + self.record_size]);
-                self.page_cache.insert(location.0, page);
+                data.copy_from_slice(&page_data[location_in_page..location_in_page + self.record_size]);
+                self.page_cache.insert(location.0, page_data);
             }
             Ok(location.0)
         }
