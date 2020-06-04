031d76d45d84e74b96316c49b14d02d283bf2631
Alexandre RICCIARDI <alexandre.ricciardi@liteapps.fr>
2020-06-04 03:13:42 +0200

    pager

--- lib/orange-db-core/src/repository/records/mod.rs
+++ lib/orange-db-core/src/repository/records/mod.rs
@@ -17,37 +17,40 @@ pub struct RecordsManager {
     page_map: PageMap,
 }
 
-trait PageRecord {
-    fn has_multi_page_record(&self) -> bool;
-    fn get_free_list_ptr(&self) -> PageId;
-    fn append_free_list_item(&self, id: PageRecordId) -> Vec<PageRecordId>;
-    fn get_page_free_list(&self) -> Vec<PageRecordId>;
-    fn get_multi_page_ptr(&self) -> PageId;
+struct RecordPageWrapper<'a> {
+    page: &'a Page<'a>,
+    page_map: PageMap,
 }
 
-impl <'a> PageRecord for Page<'a> {
+impl <'a> RecordPageWrapper<'a> {
+    fn new(page: &'a Page, page_map: PageMap) -> Self {
+        RecordPageWrapper{page: page, page_map: page_map}
+    }
     fn has_multi_page_record(&self) -> bool {
-        self.data[0] & MULTI_PAGE_RECORD_FLAG == 1
+        self.page.data[0] & MULTI_PAGE_RECORD_FLAG == 1
     }
     fn get_free_list_ptr(&self) -> PageId {
-        let mut bytes = [0u8; 8];
-        bytes.copy_from_slice(&self.data[1..9]);
+        let mut bytes = [0u8; NEXT_FREE_SLOT_PAGE_PTR_SIZE];
+        bytes.copy_from_slice(self.get_slice_ref(&self.page_map.next_free_slot_page_ptr));
         u64::from_be_bytes(bytes)
-    }
-    fn get_page_free_list(&self) -> Vec<PageRecordId> {
-        let freelist_slice = self.data[]
     }
     fn get_multi_page_ptr(&self) -> PageId {
-        let mut bytes = [0u8; 8];
-        bytes.copy_from_slice(&self.data[9..9+MULTI_PAGE_RECORD_PTR_SIZE]);
+        let mut bytes = [0u8; MULTI_PAGE_RECORD_PTR_SIZE];
+        bytes.copy_from_slice(self.get_slice_ref(&self.page_map.multi_page_recode_ptr));
         u64::from_be_bytes(bytes)
     }
-    fn append_free_list_item(&self, id: PageRecordId) -> Vec<PageRecordId> {
-
+    fn get_slice_ref(&self, bounds: &Bounds) -> &[u8] {
+        &self.page.data[bounds.begin..bounds.end]
+    }
+    fn get_free_list_len(&self) -> usize {
+        let mut bytes = [0u8; FREE_LIST_LEN_SIZE];
+        bytes.copy_from_slice(self.get_slice_ref(&self.page_map.free_list_len));
+        u16::from_be_bytes(bytes) as usize
     }
 }
 
 const FREE_LIST_PTR_SIZE: usize = 4;
+const FREE_LIST_LEN_SIZE: usize = 2;
 const MULTI_PAGE_RECORD_PTR_SIZE: usize = 8;
 const NEXT_FREE_SLOT_PAGE_PTR_SIZE: usize = 8;
 const HEADER_FLAGS: usize = 1; 
@@ -55,6 +58,7 @@ const HEADER_SIZE: usize = HEADER_FLAGS + NEXT_FREE_SLOT_PAGE_PTR_SIZE + MULTI_P
 
 const MULTI_PAGE_RECORD_FLAG: u8 = 0b1000_0000;
 
+#[derive(Debug, Copy, Clone)]
 struct Bounds {
     begin: usize,
     end: usize,
@@ -69,10 +73,12 @@ impl Bounds {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
 struct PageMap {
     header_flags: Bounds,
     next_free_slot_page_ptr: Bounds,
     multi_page_recode_ptr: Bounds,
+    free_list_len: Bounds,
     free_list: Bounds,
     payload: Bounds,
 }
@@ -95,12 +101,14 @@ fn compute_page_map(record_size: usize) -> PageMap {
     let header_flags_bounds = Bounds::new(0, HEADER_FLAGS);
     let next_free_slot_page_ptr_bounds = Bounds::new(header_flags_bounds.end, header_flags_bounds.end + NEXT_FREE_SLOT_PAGE_PTR_SIZE);
     let multi_page_recode_ptr_bounds = Bounds::new(next_free_slot_page_ptr_bounds.end, next_free_slot_page_ptr_bounds.end + MULTI_PAGE_RECORD_PTR_SIZE);
-    let free_list_bounds = Bounds::new(multi_page_recode_ptr_bounds.end, multi_page_recode_ptr_bounds.end + free_list_size);
+    let free_list_len = Bounds::new(multi_page_recode_ptr_bounds.end, multi_page_recode_ptr_bounds.end + FREE_LIST_LEN_SIZE);
+    let free_list_bounds = Bounds::new(free_list_len.end, free_list_len.end + free_list_size);
     let payload_bounds = Bounds::new(free_list_bounds.end, PAGE_SIZE);
     PageMap{
         header_flags: header_flags_bounds,
         next_free_slot_page_ptr: next_free_slot_page_ptr_bounds,
         multi_page_recode_ptr: multi_page_recode_ptr_bounds,
+        free_list_len: free_list_len,
         free_list: free_list_bounds,
         payload: payload_bounds,
     }
@@ -171,10 +179,10 @@ impl RecordsManager {
     }
 
     fn get_page_free_list(&self, page: &Page) -> Vec<PageRecordId> {
-        let free_list_slice = &page.data[self.page_map.free_list.begin..self.page_map.free_list.end];
         let mut res = Vec::new();
-        let mut count = 0;
-        loop {
+        let page_wrapper = RecordPageWrapper::new(page, self.page_map);
+        let free_list_slice = page_wrapper.get_slice_ref(&self.page_map.free_list);
+        for count in 0..page_wrapper.get_free_list_len() {
             let offset = count * FREE_LIST_PTR_SIZE;
             let mut bytes: [u8; FREE_LIST_PTR_SIZE];
             bytes.copy_from_slice(&free_list_slice[offset..offset+FREE_LIST_PTR_SIZE]);
