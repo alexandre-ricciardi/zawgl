da643011306c24c8ac8b7fb90542cc132924b29d
Alexandre RICCIARDI <alexandre.ricciardi@liteapps.fr>
2020-06-08 05:08:57 +0200

    b+tree

--- lib/orange-db-core/src/repository/index/b_tree.rs
+++ lib/orange-db-core/src/repository/index/b_tree.rs
@@ -1,6 +1,4 @@
-use super::super::io::file_access::FileAccess;
-use super::super::pager::*;
-
+use super::super::records::*;
 
 pub struct BNode {
 
@@ -8,15 +6,35 @@ pub struct BNode {
 
 
 const NB_CELL: usize = 66;
-const PTR_SIZE: usize = std::mem::size_of::<u64>();
+const PTR_SIZE: usize = 8;
 const KEY_SIZE: usize = 40;
-const CELL_SIZE: usize = KEY_SIZE + std::mem::size_of::<u64>();
-const BLOCK_SIZE: usize = CELL_SIZE * NB_CELL + PTR_SIZE;
+const CELL_SIZE: usize = KEY_SIZE + PTR_SIZE;
+const RECORD_SIZE: usize = CELL_SIZE * NB_CELL + PTR_SIZE;
 
 #[derive(Copy, Clone)]
 pub struct Cell {
     pub key: [u8; KEY_SIZE],
     pub ptr: u64,
+}
+
+impl Cell {
+    fn new() -> Self {
+        Cell{key: [0u8; KEY_SIZE], ptr: 0}
+    }
+    fn to_bytes(&self) -> [u8; CELL_SIZE] {
+        let mut bytes = [0u8; CELL_SIZE];
+        bytes[..KEY_SIZE].copy_from_slice(&self.key);
+        bytes[KEY_SIZE..].copy_from_slice(&self.ptr.to_be_bytes());
+        bytes
+    }
+    fn from_bytes(bytes: &[u8]) -> Self {
+        let key = [0u8; KEY_SIZE];
+        key.copy_from_slice(&bytes[..KEY_SIZE]);
+        let buf = [0u8; PTR_SIZE];
+        buf.copy_from_slice(&bytes[KEY_SIZE..]);
+        let ptr = u64::from_be_bytes(buf);
+        Cell{key: key, ptr: ptr}
+    }
 }
 
 #[derive(Copy, Clone)]
@@ -28,48 +46,46 @@ pub struct BNodeRecord {
 
 impl BNodeRecord {
     fn get_ptr_value(&self, n: usize) -> u64 {
-        self.ptrs[n]
+        self.cells[n].ptr
     }
-    fn get_key_value(&self, n: usize) -> u64 {
-        self.keys[n]
+    fn get_key_value(&self, n: usize) -> String {
+        String::from_utf8(self.cells[n].key.to_vec()).unwrap()
     }
-    fn to_bytes(&self) -> [u8; BLOCK_SIZE] {
-        let mut bytes = [0u8; BLOCK_SIZE];
+    fn get_keys_string(&self) -> [String; NB_CELL] {
+        let keys = [String::new(); NB_CELL];
+        for cell_id in 0..self.cells.len() {
+            keys[cell_id] = String::from_utf8(self.cells[cell_id].key.to_vec()).unwrap();
+        }
+        keys
+    }
+    fn to_bytes(&self) -> [u8; RECORD_SIZE] {
+        let mut bytes = [0u8; RECORD_SIZE];
         let mut index = 0;
         bytes[index] = self.header;
         index += 1;
-
-        for key in self.keys.iter() {
-            bytes[index..index+SLOT_SIZE].copy_from_slice(&key.to_be_bytes());
-            index += SLOT_SIZE;
+        for cell_id in 0..self.cells.len() {
+            bytes[index..index+CELL_SIZE].copy_from_slice(&self.cells[cell_id].to_bytes());
+            index += CELL_SIZE;
         }
-        for ptr in self.ptrs.iter() {
-            bytes[index..index+SLOT_SIZE].copy_from_slice(&ptr.to_be_bytes());
-            index += SLOT_SIZE;
-        }
+        bytes[index..].copy_from_slice(&self.ptr.to_be_bytes());
         bytes
     }
-    fn from_bytes(bytes: [u8; BLOCK_SIZE]) -> Self {
-        let header = bytes[0];
-        let mut keys = [0u64; NB_SLOT];
-        for index in 1..NB_SLOT+1 {
-            let mut tmp = [0u8; SLOT_SIZE];
-            tmp.copy_from_slice(&bytes[index*SLOT_SIZE..index*(SLOT_SIZE+1)]);
-            keys[index] = u64::from_be_bytes(tmp);
+    fn from_bytes(bytes: [u8; RECORD_SIZE]) -> Self {
+        let mut index = 0;
+        let header = bytes[index];
+        index += 1;
+        let cells = [Cell::new(); NB_CELL];
+        for cell_id in 0..cells.len() {
+            cells[cell_id] = Cell::from_bytes(&bytes[index..index+CELL_SIZE]);
+            index += CELL_SIZE;
         }
-        let mut ptrs = [0u64; NB_SLOT+1];
-        for index in NB_SLOT+1..BLOCK_SIZE {
-            let mut tmp = [0u8; SLOT_SIZE];
-            tmp.copy_from_slice(&bytes[index*SLOT_SIZE..index*(SLOT_SIZE+1)]);
-            ptrs[index] = u64::from_be_bytes(tmp);
-        }
-        BNodeRecord{header: header, keys: keys, ptrs: ptrs}
+        let buf = [0u8; PTR_SIZE];
+        buf.copy_from_slice(&bytes[index..]);
+        let ptr = u64::from_be_bytes(buf);
+        BNodeRecord{header: header, cells: cells, ptr: ptr}
     }
     fn len(&self) -> usize {
         (self.header & 0x00FF) as usize
-    }
-    fn is_full(&self) -> bool {
-        self.len() >= 2 * NB_ITEM
     }
     fn is_leaf(&self) -> bool {
         (self.header & 0b1000_0000) == 1
@@ -77,7 +93,7 @@ impl BNodeRecord {
 }
 
 pub struct BTreeIndex {
-    pager: Pager,
+    records_manager: RecordsManager,
     tree_depth: u32,
 }
 
@@ -85,22 +101,23 @@ pub type BTreeResult = std::result::Result<BNodeRecord, Option<BNodeRecord>>;
 
 impl BTreeIndex {
     pub fn new(file: &str, tree_depth: u32) -> Self {
-        BTreeIndex{pager: Pager::new(file, BLOCK_SIZE as u64), tree_depth: tree_depth}
+        BTreeIndex{records_manager: RecordsManager::new(file, RECORD_SIZE), tree_depth: tree_depth}
     }
 
     fn is_leaf_node(&self, depth: u32) -> bool {
         depth == self.tree_depth
     }
 
-    fn tree_search(&mut self, value: u64, node: &BNodeRecord, depth: u32) -> BTreeResult {
+    fn tree_search(&mut self, value: &str, node: &BNodeRecord, depth: u32) -> BTreeResult {
         if self.is_leaf_node(depth) {
             Ok(*node)
         } else {
-            let res = node.keys.binary_search(&value);
+            let keys = node.get_keys_string();
+            let res = keys.binary_search(&String::from(value));
             match res {
                 Ok(found) => {
-                    let mut data = [0u8; BLOCK_SIZE];
-                    self.pager.load(node.ptrs[found], &mut data).or_else(|_| Err(None))?;
+                    let mut data = [0u8; RECORD_SIZE];
+                    self.records_manager.load(node.get_ptr_value(found), &mut data);
                     let child = BNodeRecord::from_bytes(data);
                     if child.is_leaf() {
                         Ok(child)
@@ -109,8 +126,8 @@ impl BTreeIndex {
                     }
                 },
                 Err(not_found) => {
-                    let mut data = [0u8; BLOCK_SIZE];
-                    self.pager.load(node.ptrs[not_found], &mut data).or_else(|_| Err(None))?;
+                    let mut data = [0u8; RECORD_SIZE];
+                    self.records_manager.load(node.get_ptr_value(not_found), &mut data);
                     let child = BNodeRecord::from_bytes(data);
                     self.tree_search(value, &child, depth+1)
                 }
@@ -118,12 +135,12 @@ impl BTreeIndex {
         }
     }
 
-    pub fn search(&mut self, value: u64) -> BTreeResult {
-        if self.pager.is_empty() {
+    pub fn search(&mut self, value: &str) -> BTreeResult {
+        if self.records_manager.is_empty() {
             Err(None)
         } else {
-            let mut data = [0u8; BLOCK_SIZE];
-            self.pager.load(0, &mut  data).or_else(|_| Err(None))?;
+            let mut data = [0u8; RECORD_SIZE];
+            self.records_manager.load(0, &mut  data);
             self.tree_search(value, &BNodeRecord::from_bytes(data), 0)
         }
     }
