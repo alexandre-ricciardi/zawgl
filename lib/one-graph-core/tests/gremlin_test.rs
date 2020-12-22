9152ae3cfcab44e1f26081f159970676cf1ba24d
Alexandre RICCIARDI <alexandre.ricciardi@liteapps.fr>
2020-12-22 04:23:55 +0100

    tests

---
+++ lib/one-graph-core/tests/gremlin_test.rs
@@ -0,0 +1,10 @@
+extern crate gremlin_client;
+use gremlin_client::{GremlinClient, process::traversal::traversal};
+
+#[test]
+fn gremlin() {
+    let client = GremlinClient::connect("localhost").unwrap();
+    let g = traversal().with_remote(client);
+    let res = g.v(()).has_label("person").has(("name","Jon"));
+    println!("{:?}", res.bytecode());
+}
