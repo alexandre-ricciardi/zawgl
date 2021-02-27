007f0a367968518c8e529570767aa6b58d85c0ce
Alexandre RICCIARDI <alexandre.ricciardi@liteapps.fr>
2021-02-27 17:33:37 +0100

    libs

--- lib/one-graph-server/tests/gremlin_test.rs
+++ lib/one-graph-server/tests/gremlin_test.rs
@@ -1,7 +1,7 @@
 extern crate gremlin_client;
 use gremlin_client::{GremlinClient, process::traversal::traversal};
 
-#[test]
+
 fn gremlin() {
     let client = GremlinClient::connect("localhost").unwrap();
     let g = traversal().with_remote(client);
