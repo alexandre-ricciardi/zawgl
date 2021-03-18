ece24b3248c8bef2683d0076679ebd10ce218dbd
Alexandre RICCIARDI <alexandre.ricciardi@liteapps.fr>
2021-03-18 06:22:10 +0100

    .

--- lib/one-graph-gremlin/src/json_gremlin_response_builder.rs
+++ lib/one-graph-gremlin/src/json_gremlin_response_builder.rs
@@ -3,17 +3,5 @@ use serde_json::Value;
 use serde_json::json;
 
 pub fn build_json_gremlin_response(gremlin: &GremlinResponse) -> Option<Value> {
-    let mut json_res = json!({
-        "request_id": gremlin.request_id,
-        "status": {
-            "messages": "",
-            "code": 200
-        },
-        "result": {
-            "data": null,
-            "meta": null
-        }
-    });
-    
-    Some(json_res)
+    Some(gremlin.to_json())
 }
