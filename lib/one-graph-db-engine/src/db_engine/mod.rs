b6b185ac457375e46596d39372ec0a5fcb321010
Alexandre RICCIARDI <alexandre.ricciardi@liteapps.fr>
2021-03-26 07:49:47 +0100

    .

--- lib/one-graph-db-engine/src/db_engine/mod.rs
+++ lib/one-graph-db-engine/src/db_engine/mod.rs
@@ -1,4 +1,5 @@
 use one_graph_gremlin::gremlin::*;
+use one_graph_core::model::*;
 
 pub struct GraphDatabaseEngine {
     
@@ -6,6 +7,23 @@ pub struct GraphDatabaseEngine {
 
 impl GraphDatabaseEngine {
     fn handle_gremlin_request(gremlin: &GremlinRequest) -> Option<GremlinResponse> {
+        let mut pattern = PropertyGraph::new();
+        for step in &gremlin.steps {
+            match step {
+                GStep::V(id) => {
+                    match id {
+                        Some(val) => {
+                            let n = Node::new();
+                            
+                        },
+                        None => {
+
+                        }
+                    }
+                },
+                _ => {}
+            }
+        }
         None
     }
 }
