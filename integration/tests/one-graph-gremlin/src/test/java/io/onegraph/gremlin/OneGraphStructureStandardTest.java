516b8806c69d8dc32645e9233e67ad543f613781
Alexandre RICCIARDI <alexandre.ricciardi@pm.me>
2021-11-08 13:52:53 +0100

    .

--- integration/tests/one-graph-gremlin/src/test/java/io/onegraph/gremlin/OneGraphStructureStandardTest.java
+++ integration/tests/one-graph-gremlin/src/test/java/io/onegraph/gremlin/OneGraphStructureStandardTest.java
@@ -2,10 +2,12 @@ package io.onegraph.gremlin;
 
 import org.apache.tinkerpop.gremlin.GraphProviderClass;
 import org.apache.tinkerpop.gremlin.structure.StructureStandardSuite;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 public class GremlinTestSuite {
-    
+
+    @Test
     @RunWith(StructureStandardSuite.class)
     @GraphProviderClass(provider = OneGraphProvider.class, graph = OneGraph.class)
     public class OneGraphStructureStandardTest {}
