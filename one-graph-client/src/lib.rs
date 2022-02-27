23d4be7e36b6e888d98795c3df4b8174377b0709
Alexandre RICCIARDI <alexandre.ricciardi@pm.me>
2022-02-27 09:45:06 +0100

    .

--- one-graph-client/src/lib.rs
+++ one-graph-client/src/lib.rs
@@ -1,22 +1,22 @@
 use futures_util::{future, pin_mut, StreamExt};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+use bson::{Bson, Document, doc};
+use uuid::Uuid;
 
 pub struct Client {
     address: String,
 }
 
-async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
-    let mut stdin = tokio::io::stdin();
-    loop {
-        let mut buf = vec![0; 1024];
-        let n = match stdin.read(&mut buf).await {
-            Err(_) | Ok(0) => break,
-            Ok(n) => n,
-        };
-        buf.truncate(n);
-        tx.unbounded_send(Message::binary(buf)).unwrap();
-    }
+async fn send_request(tx: futures_channel::mpsc::UnboundedSender<Message>, query: &str) -> Option<()>{
+    let mut msg = "!application/openCypher".as_bytes().to_vec();
+    let doc = doc!{
+        "request_id": Uuid::new_v4().to_urn().to_string(),
+        "query" : query,
+    };
+    doc.to_writer(&mut msg).ok()?;
+    tx.unbounded_send(Message::binary(msg)).unwrap();
+    Some(())
 }
 
 
@@ -25,17 +25,16 @@ impl Client {
         Client{address: String::from(address)}
     }
 
-    pub async fn execute_cypher_request(&self) {
+    pub async fn execute_cypher_request(&self, query: &'static str) {
         let url = url::Url::parse(&self.address).unwrap();
-        let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
-        tokio::spawn(read_stdin(stdin_tx));
+        let (request_tx, response_rx) = futures_channel::mpsc::unbounded();
+        tokio::spawn(send_request(request_tx, query));
     
         let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
-        println!("WebSocket handshake has been successfully completed");
     
         let (write, read) = ws_stream.split();
     
-        let stdin_to_ws = stdin_rx.map(Ok).forward(write);
+        let stdin_to_ws = response_rx.map(Ok).forward(write);
         let ws_to_stdout = {
             read.for_each(|message| async {
                 let data = message.unwrap().into_data();
@@ -46,6 +45,4 @@ impl Client {
         pin_mut!(stdin_to_ws, ws_to_stdout);
         future::select(stdin_to_ws, ws_to_stdout).await;
     }
-
-    
 }
