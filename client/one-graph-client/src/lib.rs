2c1d1cbcc16e9424962612bfbcc406c83cc5bb0a
Alexandre RICCIARDI <alexandre.ricciardi@pm.me>
2022-02-28 10:18:27 +0100

    .

--- client/one-graph-client/src/lib.rs
+++ client/one-graph-client/src/lib.rs
@@ -5,6 +5,7 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 use bson::{Bson, Document, doc};
 use uuid::Uuid;
+use log::*;
 
 pub struct Client {
     address: String,
@@ -29,23 +30,24 @@ impl Client {
 
     pub async fn execute_cypher_request(&self, query: &'static str) {
         let url = url::Url::parse(&self.address).unwrap();
-        let (request_tx, response_rx) = futures_channel::mpsc::unbounded();
+        let (request_tx, request_rx) = futures_channel::mpsc::unbounded();
         tokio::spawn(send_request(request_tx, query));
     
         let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
     
         let (write, read) = ws_stream.split();
     
-        let stdin_to_ws = response_rx.map(Ok).forward(write);
-        let ws_to_stdout = {
+        request_rx.map(Ok).forward(write).await.expect("done");
+
+        let ws_to_log = {
             read.for_each(|message| async {
                 let data = message.unwrap().into_data();
                 let doc = Document::from_reader(Cursor::new(data)).expect("response");
-                tokio::io::stdout().write_all(doc.to_string().as_bytes()).await.unwrap();
+                trace!("received: {}", doc.to_string());
             })
         };
     
-        pin_mut!(stdin_to_ws, ws_to_stdout);
-        future::select(stdin_to_ws, ws_to_stdout).await;
+        pin_mut!(ws_to_log);
+        ws_to_log.await;
     }
 }
--- og/src/settings.rs
+++ og/src/settings.rs
@@ -26,12 +26,12 @@ pub struct Settings {
 
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
-        let mut s = Config::new();
+        let s = Config::builder();
         let mut current_dir = env::current_dir().map_err(|err| ConfigError::Message(err.to_string()))?;
         let config_file_path = Path::new(CONFIG_FILE_PATH);
         current_dir.push(config_file_path);
-        s.merge(File::from(current_dir.as_path()))?;
-        s.try_into()
+        s.add_source(config::File::with_name(current_dir.as_path().to_str().expect("config file path")))
+        .build().expect("settings").try_deserialize()
     }
 
 
