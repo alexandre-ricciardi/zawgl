32acdf776b7e63ad390496b01574533bd9ea73ee
Alexandre RICCIARDI <alexandre.ricciardi@pm.me>
2022-03-01 08:40:15 +0100

    .

--- integration/cypher-tests/tests/open_cypher_tests.rs
+++ integration/cypher-tests/tests/open_cypher_tests.rs
@@ -3,7 +3,7 @@ use std::{thread, time};
 use log::LevelFilter;
 use one_graph_core::{model::init::InitContext, test_utils::build_dir_path_and_rm_old};
 use simple_logger::SimpleLogger;
-
+use log::*;
 use one_graph_client::Client;
 
 #[tokio::test]
@@ -13,7 +13,7 @@ async fn test_cypher() {
     let ctx = InitContext::new(&db_dir).expect("can't create database context");
     let server = one_graph_server::run_server("localhost:8182", ctx);
 
-    
+    thread::sleep(time::Duration::from_millis(1000));
 
     tokio::select!{
         _ = server => 0,
@@ -23,6 +23,13 @@ async fn test_cypher() {
 
 async fn test_cypher_requests() {
     let mut client = Client::new("ws://localhost:8182").await;
-    client.execute_cypher_request("create (n:Person) return n").await;
-    client.execute_cypher_request("match (n:Person) return n").await;
+    let res0 = client.execute_cypher_request("create (n:Person) return n").await;
+    if let Ok(d) = res0 {
+        debug!("{}", d.to_string())
+    }
+   
+    let res1 = client.execute_cypher_request("match (n:Person) return n").await;
+    if let Ok(d) = res1 {
+        debug!("{}", d.to_string())
+    }
 }
