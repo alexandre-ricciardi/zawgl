fd12970ce793323b2636fe4bb46757f7f4666e41
Alexandre RICCIARDI <alexandre.ricciardi@pm.me>
2022-03-01 04:59:46 +0100

    .

--- integration/cypher-tests/tests/open_cypher_tests.rs
+++ integration/cypher-tests/tests/open_cypher_tests.rs
@@ -1,20 +1,28 @@
+use std::{thread, time};
+
 use log::LevelFilter;
 use one_graph_core::{model::init::InitContext, test_utils::build_dir_path_and_rm_old};
 use simple_logger::SimpleLogger;
 
 use one_graph_client::Client;
 
-//#[tokio::test]
+#[tokio::test]
 async fn test_cypher() {
     let db_dir = build_dir_path_and_rm_old("test_cypher").expect("error");
     SimpleLogger::new().with_level(LevelFilter::Trace).init().unwrap();
     let ctx = InitContext::new(&db_dir).expect("can't create database context");
     let server = one_graph_server::run_server("localhost:8182", ctx);
+
     
-    let client = Client::new("ws://localhost:8182");
 
     tokio::select!{
         _ = server => 0,
-        _ = client.execute_cypher_request("match (n:Person) return n") => 0,
+        _ = test_cypher_requests() => 0,
     };
+}
+
+async fn test_cypher_requests() {
+    let mut client = Client::new("ws://localhost:8182").await;
+    client.execute_cypher_request("create (n:Person) return n").await;
+    client.execute_cypher_request("match (n:Person) return n").await;
 }
