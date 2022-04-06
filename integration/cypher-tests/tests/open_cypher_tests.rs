95bfc8a7524d8604c94f58a3cae6e4c32d8a8a4c
Alexandre RICCIARDI <alexandre.ricciardi@pm.me>
2022-04-06 10:13:18 +0200

    Wip (#10)
    * .
    
    * todo fix create
    
    * fix create
    
    * .

--- integration/cypher-tests/tests/open_cypher_tests.rs
+++ integration/cypher-tests/tests/open_cypher_tests.rs
@@ -3,11 +3,18 @@ use one_graph_core::{model::init::InitContext, test_utils::build_dir_path_and_rm
 use simple_logger::SimpleLogger;
 use log::*;
 use og_client::Client;
+use std::{future::Future, rc::Rc};
+use bson::{Bson, Document, doc};
 
 #[tokio::test]
 async fn test_cypher() {
-    let db_dir = build_dir_path_and_rm_old("test_cypher").expect("error");
-    SimpleLogger::new().with_level(LevelFilter::Debug).init().unwrap();
+    run_test("first_test", test_cypher_requests).await;
+    text_double_create_issue().await;
+}
+
+async fn run_test<F, T>(db_name: &str, lambda: F) where F : FnOnce() -> T, T : Future<Output = ()> + Send {
+    let db_dir = build_dir_path_and_rm_old(db_name).expect("error");
+    
     let ctx = InitContext::new(&db_dir).expect("can't create database context");
     let (tx, rx) = tokio::sync::oneshot::channel::<()>();
     let server = one_graph_server::run_server("localhost:8182", ctx, || {
@@ -17,38 +24,78 @@ async fn test_cypher() {
     });
     tokio::spawn(server);
     match rx.await {
-        Ok(_) => test_cypher_requests().await,
+        Ok(_) => lambda().await,
         Err(_) => error!("starting database"),
     }
 }
 
 async fn test_cypher_requests() {
     let mut client = Client::new("ws://localhost:8182").await;
-    let res0 = client.execute_cypher_request("create (n:Person) return n").await;
-    if let Ok(d) = res0 {
+    let r = client.execute_cypher_request("create (n:Person) return n").await;
+    if let Ok(d) = r {
         debug!("{}", d.to_string())
     }
-   
-    let res1 = client.execute_cypher_request("match (n:Person) return n").await;
-    if let Ok(d) = res1 {
-        debug!("{}", d.to_string())
-    }
-
-    let r2 = client.execute_cypher_request("create (n:Movie) return n").await;
-    if let Ok(d) = r2 {
+    let r = client.execute_cypher_request("create (n:Movie) return n").await;
+    if let Ok(d) = r {
         debug!("{}", d.to_string())
     }
 
     
-    let r3 = client.execute_cypher_request("create (n:Movie)<-[r:Played]-(p:Person) return n, r, p").await;
-    if let Ok(d) = r3 {
+    let r = client.execute_cypher_request("create (n:Movie)<-[r:Played]-(p:Person) return n, r, p").await;
+    if let Ok(d) = r {
         debug!("{}", d.to_string())
     }
 
     
-    let r4 = client.execute_cypher_request("match (p:Person), (m:Movie) create (m)<-[r:Played]-(p) return m, r, p").await;
-    if let Ok(d) = r4 {
+    let r = client.execute_cypher_request("match (n:Person) return n").await;
+    if let Ok(d) = r {
+        let res = d.get_document("result").expect("result");
+        let graphs = res.get_array("graphs").expect("graphs");
+        assert_eq!(graphs.len(), 2);
+        for g in graphs {
+            let graph = g.as_document().expect("a graph");
+            let nodes = graph.get_array("nodes").expect("nodes");
+            assert_eq!(nodes.len(), 1);
+        }
+    } else {
+        assert!(false, "no response")
+    }
+    
+    let r = client.execute_cypher_request("match (n:Movie) return n").await;
+    if let Ok(d) = r {
+        let res = d.get_document("result").expect("result");
+        let graphs = res.get_array("graphs").expect("graphs");
+        assert_eq!(graphs.len(), 2);
+        for g in graphs {
+            let graph = g.as_document().expect("a graph");
+            let nodes = graph.get_array("nodes").expect("nodes");
+            assert_eq!(nodes.len(), 1);
+        }
+    } else {
+        assert!(false, "no response")
+    }
+    
+    let r = client.execute_cypher_request("match (p:Person), (m:Movie) create (m)<-[r:Played]-(p) return m, r, p").await;
+    if let Ok(d) = r {
         debug!("{}", d.to_string())
     }
+}
 
+async fn text_double_create_issue() {
+    run_test("another_test", move || async {
+        let mut client = Client::new("ws://localhost:8182").await;
+        let r3 = client.execute_cypher_request("create (n:Movie)<-[r:Played]-(p:Person) return n, r, p").await;
+        if let Ok(d) = r3 {
+            debug!("{}", d.to_string());
+            let res = d.get_document("result").expect("result");
+            let graphs = res.get_array("graphs").expect("graphs");
+            for g in graphs {
+                let graph = g.as_document().expect("a graph");
+                let nodes = graph.get_array("nodes").expect("nodes");
+                assert_eq!(nodes.len(), 2);
+            }
+        } else {
+            assert!(false, "no response")
+        }
+    }).await;
 }
