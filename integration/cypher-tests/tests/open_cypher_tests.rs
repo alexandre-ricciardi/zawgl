6a2526ee7ec874f0c1b3cba325bada7e310e9515
Alexandre RICCIARDI <alexandre.ricciardi@pm.me>
2022-08-10 07:03:24 +0200

    .

--- integration/cypher-tests/tests/open_cypher_tests.rs
+++ integration/cypher-tests/tests/open_cypher_tests.rs
@@ -8,12 +8,12 @@ use std::future::Future;
 #[tokio::test]
 async fn test_cypher_0() {
     SimpleLogger::new().with_level(LevelFilter::Debug).init().unwrap();
-    //run_test("first_test", 8183, test_cypher_requests).await;
-    //run_test("create_path_test", 8184, test_create_path).await;
-    //run_test("another_test", 8185, test_double_create_issue).await;
-    //run_test("test_cypher_requests_2", 8186, test_cypher_requests_2).await;
-    //run_test("test_mutliple_match", 8187, test_mutliple_match).await;
-    //run_test("test_cypher_self_relationship", 8189, test_cypher_self_relationship).await;
+    run_test("first_test", 8183, test_cypher_requests).await;
+    run_test("create_path_test", 8184, test_create_path).await;
+    run_test("another_test", 8185, test_double_create_issue).await;
+    run_test("test_cypher_requests_2", 8186, test_cypher_requests_2).await;
+    run_test("test_mutliple_match", 8187, test_mutliple_match).await;
+    run_test("test_cypher_self_relationship", 8189, test_cypher_self_relationship).await;
     run_test("test_cypher_self_relationship_2", 8190, test_cypher_self_relationship_2).await;
 }
 
@@ -129,7 +129,7 @@ async fn test_cypher_requests_2(mut client: Client) {
         debug!("{}", d.to_string());
         let res = d.get_document("result").expect("result");
         let graphs = res.get_array("graphs").expect("graphs");
-        assert_eq!(graphs.len(), 100);
+        assert_eq!(graphs.len(), 45);
         for g in graphs {
             let graph = g.as_document().expect("a graph");
             let nodes = graph.get_array("nodes").expect("nodes");
@@ -168,7 +168,7 @@ async fn test_cypher_self_relationship_2(mut client: Client) {
         debug!("{}", d.to_string())
     }
 
-    let r = client.execute_cypher_request("match (x:Person), (y:Person) create (x)-[f:FRIEND_OF]->(y) return f").await;
+    let r = client.execute_cypher_request("match (x:Person) create (x)-[f:FRIEND_OF]->(x) return f").await;
     if let Ok(d) = r {
         debug!("{}", d.to_string());
         let res = d.get_document("result").expect("result");
