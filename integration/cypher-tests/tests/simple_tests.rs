71c7605da31d1de677ce2892ffbbd712878fd258
Alexandre RICCIARDI <aricciardi501@gmail.com>
2024-10-06 17:59:51 +0200

    fix db path for simple test demo

--- integration/cypher-tests/tests/simple_tests.rs
+++ integration/cypher-tests/tests/simple_tests.rs
@@ -20,13 +20,14 @@
 
 
 use log::LevelFilter;
-use zawgl_core::{model::{init::InitContext, Node, Property, Relationship}, repository::graph_repository::GraphRepository};
+use zawgl_core::{graph_engine::GraphEngine, model::{init::InitContext, Node, Property, PropertyGraph, Relationship}, repository::graph_repository::GraphRepository, test_utils::build_dir_path_and_rm_old};
 use simple_logger::SimpleLogger;
 
 #[test]
 fn demo() {
     SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
-    let ctx: InitContext = InitContext::new("zawgl-db").expect("can't create database context");
+    let db_dir = build_dir_path_and_rm_old("simple_test").expect("error");
+    let ctx: InitContext = InitContext::new(&db_dir).expect("can't create database context");
     let mut gr = GraphRepository::new(ctx);
     let mut alice = Node::new();
     alice.set_labels(vec!["Person".to_string()]);
@@ -41,4 +42,18 @@ fn demo() {
     gr.create_relationship(&is_friend_rel, alice_with_id.get_id().unwrap(), bob_with_id.get_id().unwrap());
     gr.sync();
 
+
+    let mut pattern = PropertyGraph::new();
+    let source_index = pattern.add_node(Node::new());
+    let target_index = pattern.add_node(Node::new());
+    pattern.add_relationship(Relationship::new(), source_index, target_index);
+
+    let mut ge = GraphEngine::from_repository(gr);
+    let results = ge.match_pattern(&pattern);
+
+    if let Some(res) = results {
+        assert_eq!(res.len(), 1);
+    } else {
+        assert!(false);
+    }
 }
