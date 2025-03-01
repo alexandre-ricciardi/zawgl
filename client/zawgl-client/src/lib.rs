1a94058e0ed7cd0be99a46b51948464e67bc3a30
Alexandre RICCIARDI <aricciardi501@gmail.com>
2025-03-01 10:04:14 +0100

    reconnect

--- client/zawgl-client/src/lib.rs
+++ client/zawgl-client/src/lib.rs
@@ -18,47 +18,47 @@ pub struct Client {
     request_tx: UnboundedSender<Message>,
     map_rx_channels: SharedChannelsMap,
     address: String,
+    is_connection_established: bool,
 }
 
 impl Client {
 
     pub async fn new(address: &str) -> Self {
-        let (ws_stream, _) = connect_async(address).await.expect("Failed to connect");
-        let (write, read) = ws_stream.split();
         let (request_tx, request_rx) = futures_channel::mpsc::unbounded();
-        
-        tokio::spawn(request_rx.map(Ok).forward(write));
         let map: SharedChannelsMap = Arc::new(Mutex::new(HashMap::new()));
-        
-        
-        let clone = Arc::clone(&map);
-        
-        tokio::spawn(async move {
-            read.for_each(|message| receive(message, Arc::clone(&map))).await
-        });
-        Client{request_tx, map_rx_channels: Arc::clone(&clone), address: address.to_string()}
+        let mut is_connection_established = false;
+        if let Ok((ws_stream, _)) = connect_async(address).await {
+            let (write, read) = ws_stream.split();
+            tokio::spawn(request_rx.map(Ok).forward(write));
+            let clone = Arc::clone(&map);
+            tokio::spawn(async move {
+                read.for_each(|message| receive(message, Arc::clone(&clone))).await
+            });
+            is_connection_established = true;
+        }
+        Client{request_tx, map_rx_channels: Arc::clone(&map), address: address.to_string(), is_connection_established}
     }
 
-
-
-    pub async fn reconnect(&mut self) {
-        let (ws_stream, _) = connect_async(&self.address).await.expect("Failed to connect");
-        let (write, read) = ws_stream.split();
-
+    pub async fn try_reconnect(&mut self) {
         let (request_tx, request_rx) = futures_channel::mpsc::unbounded();
-        
-        tokio::spawn(request_rx.map(Ok).forward(write));
-
-        let clone = Arc::clone(&self.map_rx_channels);
-        tokio::spawn(async move {
-            read.for_each(|message| receive(message, Arc::clone(&clone))).await
-        });
-        self.request_tx.close_channel();
-        self.request_tx = request_tx;
+        if let Ok((ws_stream, _)) = connect_async(self.address.to_string()).await {
+            let (write, read) = ws_stream.split();
+            tokio::spawn(request_rx.map(Ok).forward(write));
+            let clone = Arc::clone(&self.map_rx_channels);
+            tokio::spawn(async move {
+                read.for_each(|message| receive(message, Arc::clone(&clone))).await
+            });
+            self.request_tx.close_channel();
+            self.request_tx = request_tx;
+            self.is_connection_established = true;
+        }
     }
-    
+
     /// Executes a cypher request with parameters
     pub async fn execute_cypher_request_with_parameters(&mut self, db: &str, query: &str, params: Value) -> Result<Value, Canceled> {
+        if !self.is_connection_established {
+            self.try_reconnect().await;
+        }
         let uuid =  Uuid::new_v4();
         let (tx, rx) = futures_channel::oneshot::channel::<Value>();
         self.map_rx_channels.lock().unwrap().insert(uuid.to_string(), tx);
